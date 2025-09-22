50874fec50bbfbf6f01b4d3cd9738d9b35e3d784
GabrielVidigal1 <gabrielcv1@al.insper.edu.br>
2025-09-22 02:26:49 -0300

    feito?

--- exe5/main.c
+++ exe5/main.c
@@ -10,14 +10,12 @@
 
 const int BTN_PIN_R = 28;
 const int BTN_PIN_Y = 21;
-
 const int LED_PIN_R = 5;
 const int LED_PIN_Y = 10;
 
-QueueHandle_t xQueueCommands;
-
-SemaphoreHandle_t xSemaphore_r;
-SemaphoreHandle_t xSemaphore_y;
+QueueHandle_t xQueueBtn;
+SemaphoreHandle_t xSemaphoreLedR;
+SemaphoreHandle_t xSemaphoreLedY;
 
 typedef struct {
     int led_pin;
@@ -27,9 +25,9 @@ typedef struct {
 void btn_callback(uint gpio, uint32_t events) {
     if (events == GPIO_IRQ_EDGE_FALL) {
         if (gpio == BTN_PIN_R) {
-            xSemaphoreGiveFromISR(xSemaphore_r, 0);
+            xSemaphoreGiveFromISR(xSemaphoreLedR, NULL);
         } else if (gpio == BTN_PIN_Y) {
-            xSemaphoreGiveFromISR(xSemaphore_y, 0);
+            xSemaphoreGiveFromISR(xSemaphoreLedY, NULL);
         }
     }
 }
@@ -42,11 +40,10 @@ void btn_r_task(void *p) {
 
     bool is_blinking = false;
     while (true) {
-        if (xSemaphoreTake(xSemaphore_r, portMAX_DELAY)) {
+        if (xSemaphoreTake(xSemaphoreLedR, portMAX_DELAY)) {
             is_blinking = !is_blinking;
-            
             led_command_t cmd = { .led_pin = LED_PIN_R, .status = is_blinking };
-            xQueueSend(xQueueCommands, &cmd, 0);
+            xQueueSend(xQueueBtn, &cmd, 0);
         }
     }
 }
@@ -59,11 +56,10 @@ void btn_y_task(void *p) {
 
     bool is_blinking = false;
     while (true) {
-        if (xSemaphoreTake(xSemaphore_y, portMAX_DELAY)) {
+        if (xSemaphoreTake(xSemaphoreLedY, portMAX_DELAY)) {
             is_blinking = !is_blinking;
-            
             led_command_t cmd = { .led_pin = LED_PIN_Y, .status = is_blinking };
-            xQueueSend(xQueueCommands, &cmd, 0);
+            xQueueSend(xQueueBtn, &cmd, 0);
         }
     }
 }
@@ -79,7 +75,7 @@ void led_handler_task(void *p) {
     led_command_t received_cmd;
 
     while (true) {
-        if (xQueueReceive(xQueueCommands, &received_cmd, 0)) {
+        if (xQueueReceive(xQueueBtn, &received_cmd, 0)) {
             if (received_cmd.led_pin == LED_PIN_R) {
                 r_is_blinking = received_cmd.status;
                 if (!r_is_blinking) {
@@ -93,23 +89,13 @@ void led_handler_task(void *p) {
             }
         }
 
-       
-        if (r_is_blinking) {
-            gpio_put(LED_PIN_R, 1);
-        }
-        if (y_is_blinking) {
-            gpio_put(LED_PIN_Y, 1);
-        }
+        if (r_is_blinking) gpio_put(LED_PIN_R, 1);
+        if (y_is_blinking) gpio_put(LED_PIN_Y, 1);
         
         vTaskDelay(pdMS_TO_TICKS(100));
 
-        
-        if (r_is_blinking) {
-            gpio_put(LED_PIN_R, 0);
-        }
-        if (y_is_blinking) {
-            gpio_put(LED_PIN_Y, 0);
-        }
+        if (r_is_blinking) gpio_put(LED_PIN_R, 0);
+        if (y_is_blinking) gpio_put(LED_PIN_Y, 0);
         
         vTaskDelay(pdMS_TO_TICKS(100));
     }
@@ -118,10 +104,9 @@ void led_handler_task(void *p) {
 int main() {
     stdio_init_all();
     
-    xSemaphore_r = xSemaphoreCreateBinary();
-    xSemaphore_y = xSemaphoreCreateBinary();
-
-    xQueueCommands = xQueueCreate(10, sizeof(led_command_t));
+    xSemaphoreLedR = xSemaphoreCreateBinary();
+    xSemaphoreLedY = xSemaphoreCreateBinary();
+    xQueueBtn = xQueueCreate(10, sizeof(led_command_t));
 
     xTaskCreate(led_handler_task, "LED_Handler_Task", 256, NULL, 1, NULL);
     xTaskCreate(btn_r_task, "BTN_Task_R", 256, NULL, 1, NULL);
